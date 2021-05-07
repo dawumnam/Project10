f947ee2d5518afd663dd233ecd3a5d567dd81e8a
Dawum Nam <dawumnam@gmail.com>
2021-05-07 11:47:02 -0800

    complete

---
+++ Project10/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Dawum Nam on 5/7/21.
+//
+
+import UIKit
+
+class Person: NSObject {
+    
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
---
+++ Project10/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Dawum Nam on 5/7/21.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var name: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    
+}
